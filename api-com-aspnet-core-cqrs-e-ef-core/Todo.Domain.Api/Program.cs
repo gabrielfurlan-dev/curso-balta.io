17919ab0a69d9ed5417b22d5cf27a2e351553294
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2023-01-04 18:02:10 -0300

    fix

--- api-com-aspnet-core-cqrs-e-ef-core/Todo.Domain.Api/Program.cs
+++ api-com-aspnet-core-cqrs-e-ef-core/Todo.Domain.Api/Program.cs
@@ -10,46 +10,33 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddControllers();
-
-        // builder.Services.AddDbContext<DataContext>(opt => opt.USeSqlServer(Configuration.GetConnectionString("connectionString")));
-        builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-        
+        builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer("Data Source=DESKTOP-2BDQ1GT;Initial Catalog=ToDos;User ID=sa; pwd=adrvsc"));
+        // builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
         builder.Services.AddTransient<IToDoRepository, ToDoRepository>();
         builder.Services.AddTransient<CreateToDoHandler, CreateToDoHandler>();
         builder.Services.AddTransient<MarkTodoAsDoneHandler , MarkTodoAsDoneHandler >();
         builder.Services.AddTransient<MarkTodoAsUndoneHandler , MarkTodoAsUndoneHandler >();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseDeveloperExceptionPage();
         }
-
         app.UseHttpsRedirection();
-
         app.UseRouting();
-
         app.UseCors(x => x.AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowAnyOrigin());
-
         app.UseAuthorization();
         app.UseAuthorization();
-
         app.UseEndpoints(endpoints => endpoints.MapControllers());
-
         app.MapControllers();
-
         app.Run();
     }
 }
